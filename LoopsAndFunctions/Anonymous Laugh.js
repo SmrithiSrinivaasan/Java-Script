dd3791c8a7458ec39923b177b88468ab64cf1747
SmrithiSrinivaasan <smrithi2006@gmail.com>
2020-01-28 16:35:01 +0530

    Remove .git

---
+++ LoopsAndFunctions/Anonymous Laugh.js
@@ -0,0 +1,16 @@
+/*
+ * Programming Quiz: Anonymous Function - Laugh (5-4)
+ */
+
+var laugh = function(num)
+{
+    var ha = "";
+    for (var i = 1 ; i<=num ; i++)
+    {
+        ha += "ha";
+    }
+    ha += "!";
+    return ha;
+}
+
+console.log(laugh(10));
---
+++ LoopsAndFunctions/Inline Emotions.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+emotions("happy",function laugh(num) 
+                { var ha = "";
+                for (var i = 1 ; i<=num ; i++)
+                {
+                    ha += "ha";
+                }
+                ha += "!";
+                return ha;
+                });
